b885168448dac6da438525a8b48176c297ff1402
PONDHALF <private.pondhalf@gmail.com>
2026-09-25 14:39:41 +0700

    Initial Commit

---
+++ MoodPlay/MoodPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodPlayApp.swift
+//  MoodPlay
+//
+//  Created by Thanyaphat Kanokpiyasawad on 25/9/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct MoodPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
